c244404412ed6b841046013ba32c79ff7f869d49
braxtonhaynie <bhaynie@vols.utk.edu>
2020-04-05 13:31:47 -0400

    Switch and date added to task screen

--- To-DoList/To-DoList/TaskDetailViewController.swift
+++ To-DoList/To-DoList/TaskDetailViewController.swift
@@ -2,6 +2,25 @@
 import UIKit
 
 class TaskDetailViewController: UIViewController {
+   
+
+   @IBOutlet weak var Reminder_Outlet: UISwitch!
+   @IBAction func switch_Remider(_ sender: UISwitch) {
+       
+       if Reminder_Outlet.isOn {
+           print("on\n")
+       }
+       else {
+           print("wassup\n");
+       }
+   }
+    
+    @IBOutlet weak var Completion_Date: UIDatePicker!
+    @IBAction func Date_Picker(_ sender: UIDatePicker) {
+        
+        
+    }
+    
     @IBOutlet weak var taskName: UITextField!
     
     var name:String = ""
@@ -19,6 +38,5 @@ class TaskDetailViewController: UIViewController {
         }
     }
     
-
    
 }
